7b969d953ab08671e9db2b29a9e0d18daaa21b0d
Proustibat <jennifer.proust@gmail.com>
2017-11-28 15:13:23 +0100

    feat(2.4): Notre premier component

--- Projet 1 Markdown/markdown/src/index.js
+++ Projet 1 Markdown/markdown/src/index.js
@@ -1,3 +1,16 @@
-// Ici on code notre App
-console.log(document);
-document.querySelector('body').innerHTML = '<h1>Hello world</h1>';
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render () {
+        return (
+            <h1>Hello world</h1>
+        );
+    }
+}
+
+render(
+    <App/>,
+    document.getElementById('root')
+);
+
