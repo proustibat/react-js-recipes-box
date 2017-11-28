eb9e24d6aec185b172c4f8d02deedad61f7d6c88
Proustibat <jennifer.proust@gmail.com>
2017-11-28 15:19:42 +0100

    feat(2.5): Ecrire du HTML avec JSX

--- Projet 1 Markdown/markdown/src/index.js
+++ Projet 1 Markdown/markdown/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// css
+import './style/css/bootstrap.min.css';
+import './index.css';
+
 class App extends React.Component {
     render () {
         return (
-            <h1>Hello world</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea className="form-control" rows="35" value="Enter your text"></textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1>Resultat</h1>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
