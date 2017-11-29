8e5ccc3b0363ca1e451d0159280deb9c0b9a85a8
Proustibat <jennifer.proust@gmail.com>
2017-11-29 14:48:20 +0100

    feat(3.16): Utiliser les events

--- Projet 2 chatBox/chatBox/src/components/connexion.js
+++ Projet 2 chatBox/chatBox/src/components/connexion.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+
 class Connexion extends React.Component {
+
+    goToChat( event ) {
+        // recuperer le pseudo
+        console.log(this.pseudoInput.value);
+        event.preventDefault();
+    };
 
     render() {
         return (
-            <div className="connexionBox">
+            <div className="connexionBox" onSubmit={ this.goToChat.bind(this) }>
                 <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required />
+                    <input type="text"
+                        placeholder="Pseudo"
+                        required
+                        ref={ input => { this.pseudoInput = input } }
+                    />
                     <button type="submit">Go</button>
                 </form>
             </div>
