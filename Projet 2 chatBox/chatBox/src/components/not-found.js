52afcf4337f0192b8c8b72436b7820f3f9df7f32
Proustibat <jennifer.proust@gmail.com>
2017-11-29 12:24:16 +0100

    feat(3.15): Decouvrir React Router

---
+++ Projet 2 chatBox/chatBox/src/components/not-found.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <h2 className="notFound">Y a rien ici !</h2>
+    );
+};
+export default NotFound;
--- Projet 2 chatBox/chatBox/src/index.js
+++ Projet 2 chatBox/chatBox/src/index.js
@@ -4,12 +4,28 @@ import { render }   from 'react-dom';
 // components
 import App from './components/app';
 import Connexion from './components/connexion';
+import NotFound from './components/not-found';
+
+// router
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 // css
 import './index.css';
 
 
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={Connexion} />
+                <Match pattern="/pseudo/:pseudo" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    );
+};
+
 render(
-    <App />,
+    <Root />,
     document.getElementById('root')
 );
